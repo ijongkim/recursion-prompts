0f19142e514053b009550743f5dcf940992a5b6f
Innie Kim <innie.jong.kim@gmail.com>
2017-02-16 20:25:38 -0800

    Complete arraySum()

--- src/recursion.js
+++ src/recursion.js
@@ -35,6 +35,15 @@ var sum = function(array) {
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  var sum = 0;
+  for (var i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      sum += arraySum(array[i]);
+    } else {
+      sum += array[i];
+    }
+  }
+  return sum;
 };
 
 // 4. Check if a number is even.
