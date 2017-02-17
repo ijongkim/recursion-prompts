c9f5906e41b9351f590379665a76f56754d5e53e
Innie Kim <innie.jong.kim@gmail.com>
2017-02-16 19:20:59 -0800

    Complete factorial() and sum()

--- src/recursion.js
+++ src/recursion.js
@@ -7,11 +7,29 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+
+  if (n === 0) {
+    return 1;
+  } else if (n < 0) {
+    return null;
+  }
+
+  // Return result of factorial (n - 1)
+  return n * factorial(n - 1);
+
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  var copy = array.slice();
+
+  if (array.length === 0) {
+    return 0;
+  }
+
+  return copy.pop() + sum(copy);
+
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
