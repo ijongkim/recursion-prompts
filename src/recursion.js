106e550427133c491d808ac3d1e445a0316da4a5
Innie Kim <innie.jong.kim@gmail.com>
2017-02-19 19:56:30 -0800

    Complete Modulo

--- src/recursion.js
+++ src/recursion.js
@@ -181,6 +181,42 @@ var palindrome = function(string) {
 // modulo(17,5) // 2
 // modulo(22,6) // 4
 var modulo = function(x, y) {
+
+  if (y  === 0) {
+    return NaN;
+  } else if (x === 0) {
+    return 0;
+  }
+
+  if (x > 0) {
+    if (y > 0) {
+      if (x < y) {
+        return x;
+      }
+      return modulo((x-y), y);
+    } else {
+      if ((x + y) < 0) {
+        return x;
+      }
+      return modulo((x+y), y);
+    }
+
+  } else {
+
+    if (y > 0) {
+      if ((x + y) > 0) {
+        return x;
+      }
+      return modulo((x+y), y);
+    } else {
+      if ((x - y) > 0) {
+        return x;
+      }
+      return modulo((x-y), y);
+    }
+  }
+
+
 };
 
 // 12. Write a function that multiplies two numbers without using the * operator or
