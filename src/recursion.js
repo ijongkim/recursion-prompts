ba4bbdc33f92ca492ae2ef2c87c9eecc5f8dc6e3
Innie Kim <innie.jong.kim@gmail.com>
2017-02-19 23:45:04 -0800

    Complete reverseArr()

--- src/recursion.js
+++ src/recursion.js
@@ -264,6 +264,11 @@ var createArray = function(str) {
 
 // 17. Reverse the order of an array
 var reverseArr = function(array) {
+  if (array.length === 1) {
+    return [array[0]];
+  } else {
+    return [array[array.length - 1]].concat(reverseArr(array.slice(0, array.length - 1)));
+  }
 };
 
 // 18. Create a new array with a given value and length.
