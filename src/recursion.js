7ef3f589bccd43ab8884c0d1b6eae5cd3b4cd30a
Innie Kim <innie.jong.kim@gmail.com>
2017-02-20 00:14:33 -0800

    Complete countValuesInObj()

--- src/recursion.js
+++ src/recursion.js
@@ -350,6 +350,18 @@ var countKeysInObj = function(obj, key) {
 // countValuesInObj(obj, 'r') // 2
 // countValuesInObj(obj, 'e') // 1
 var countValuesInObj = function(obj, value) {
+  var total = 0;
+
+  for (key in obj) {
+    if (obj[key] === value) {
+      total++;
+    }
+    if (typeof obj[key] === 'object') {
+      total += countValuesInObj(obj[key], value);
+    }
+  }
+
+  return total;
 };
 
 // 24. Find all keys in an object (and nested objects) by a provided name and rename
