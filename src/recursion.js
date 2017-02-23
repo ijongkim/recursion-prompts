3a05a1ff86d14b040454790bb2b6d8f536d2d3b6
Innie Kim <innie.jong.kim@gmail.com>
2017-02-22 23:40:52 -0800

    Complete letterTally()

--- src/recursion.js
+++ src/recursion.js
@@ -489,6 +489,21 @@ var flatten = function(array) {
 // 31. Given a string, return an object containing tallies of each letter.
 // letterTally('potato'); // {p:1, o:2, t:2, a:1}
 var letterTally = function(str, obj) {
+  var letter = str.charAt(0);
+  var count = obj || {};
+
+  if (letter in count) {
+    count[letter]++;
+  } else {
+    count[letter] = 1;
+  }
+
+  if (str.length > 1) {
+    return letterTally(str.slice(1), count);
+  } else {
+    return count;
+  }
+
 };
 
 // 32. Eliminate consecutive duplicates in a list. If the list contains repeated
