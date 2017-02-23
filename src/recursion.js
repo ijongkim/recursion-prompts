6895bcd06b97f908f4ae586185b929de80f8c6a2
Innie Kim <innie.jong.kim@gmail.com>
2017-02-22 23:18:39 -0800

    Complete capitalizeFirst()

--- src/recursion.js
+++ src/recursion.js
@@ -439,6 +439,16 @@ var capitalizeWords = function(array) {
 // 28. Given an array of strings, capitalize the first letter of each index.
 // capitalizeFirst(['car','poop','banana']); // ['Car','Poop','Banana']
 var capitalizeFirst = function(array) {
+  var word = array[0];
+  var firstLetter = word.charAt(0).toUpperCase();
+  word = [firstLetter + word.slice(1)];
+
+  if (array.length > 1) {
+    return word.concat(capitalizeFirst(array.slice(1)));
+  } else {
+    return word;
+  }
+
 };
 
 // 29. Return the sum of all even numbers in an object containing nested objects.
